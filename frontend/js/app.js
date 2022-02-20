63c2bf22b835ea40518593f847e418686f952425
Fibre44 <denis.nicolas@hotmail.fr>
2022-02-20 22:01:10 +0100

    Mise en place de la récupération des éléments du transfert et premier élément pour le post

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -9,11 +9,8 @@ function getCredentials (){
 
     const domainLogin = document.getElementsByClassName("step__form__identifiant");
     const password = document.getElementsByClassName("step__form__password");
-
     const credentials = domainLogin[0].value+':'+password[0].value
-
     return credentials
-
 }
 
 function getFolderIn (){
@@ -21,7 +18,6 @@ function getFolderIn (){
   const folderIn = document.getElementById("folderIn").value;
   const folderInRename = renameFolder(folderIn)
   return folderInRename;
-
 }
 
 
@@ -146,9 +142,53 @@ function getData(patch){
         
       })
     
-    
-
 }
+
+/**
+ * Fonction pour créer un salarié
+ * Etape 1 création d'un objet avec les éléments de la séléction
+ * Etape 2 getIdentity pour obtenir les informations
+ * Etape 3 Modification des données de du retour de getIdentity pour adapter au post Employee retour du matricule
+ * Etape 4 Mise à jour des informations manquantes 
+ * Etape 5 Récupération du RIB + création dans la nouvelle société
+ * Etape 6 Si analytique récupération
+ * Etape 7 Si zones libres identiques reprise
+ * @returns 
+ */
+
+function migration(){
+  button[5].addEventListener("click", function(){
+    options = migrationOptions();
+
+    console.log(options);
+
+    getData("https://y2cbrh-ondemand.cegid.com//CegidRHWebApi/v1/Employee/OData/Identity?folderId="+getFolderIn())
+    .then((employees) => {
+
+      //récupération du matricule
+      const employeesJSON = JSON.parse(employees);
+      const matriculeSearch =  employeesJSON.Items.find(employees => employees.EmployeeId == options.matricule);
+      console.log(matriculeSearch);
+
+      return matriculeSearch
+
+
+    })
+    .catch((error) => {
+      console.error("Erreur lors de la recherche du salarié",error);
+    })
+    .then((matricule)=> {
+      const newEmployee = createEmployee(matricule,options.establishment,options.date)
+      console.log(newEmployee);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  })
+  
+}
+
+migration();
 
 /**
  * 
@@ -219,7 +259,6 @@ function writeEstablishments (establishments){
     selectEstablishment.appendChild(optionEstablishment);
   }
 
-
 }
 
 /**
@@ -242,6 +281,151 @@ function buttonValidation(localisation){
 
   localisation.classList.add("button__validation")
   localisation.disabled = true;
+}
+
+
+
+function postEmployee (){
+
+
+  paramConnexion = {
+    login : getCredentials(),
+    url : patch,
+  }
+
+    fetch(urlServer+"/apiCegid/post",{
+      method: "POST",
+      headers:{
+        'Accept':'application/json',
+        'Content-Type': 'application/json'
+      ,
+     },
+       body: JSON.stringify(paramConnexion)
+ 
+     })
+     .then (function (res){
+       if (res.ok){
+         return(res.json());
+       }    
+     })
+
+     
+     .catch(function(err){
+       res.status;
+     }); 
+    
+
+}
+
+/**
+ * 
+ * @returns un objet avec les éléments selectionnés par l'utilisateur
+ */
+
+function migrationOptions(){
+
+  options = {
+
+    folderIn : getFolderIn(),
+    folderOut : getFolderOut(),
+    matricule : getMatricule(),
+    establishment : getEstablishment(),
+    date : getDate()
+
+  }
+
+  return (options)
+
+}
+
+/**
+ * @returns le matricule selectionné
+ */
+function getMatricule(){
+
+  const matriculeComplet = document.getElementById("employees").value;
+  const matriculeSplit = matriculeComplet.split(' ');
+  const matricule = matriculeSplit[0]
+
+  return matricule;
+}
+
+/**
+ * 
+ * @returns l'etablissement séléctionné
+ */
+
+function getEstablishment(){
+
+  const establishmentComplet = document.getElementById("establisments").value;
+  const establishmentSplit = establishmentComplet.split(" ");
+  establishment = establishmentSplit[0];
+
+  return establishment;
+}
+
+function getDate(){
+
+  const date = document.getElementById("date").value;
+  return date;
+}
+
+/**
+ * 
+ * @param {JSON} employee 
+ * @param {string} code etablissement
+ * @param {date} date du transfert
+ * @return un employé au format du post
+ */
+
+function createEmployee(employee,establishment,date){
+
+  const newEmployee = {
+
+      Name: employee.Name,
+      MaidenName: "",
+      FirstName: employee.FirstName,
+      Civility: employee.Civility,
+      Sex: employee.Gender,
+      Address: employee.Adress1,
+      Address2: employee.Adress2,
+      Address3: employee.Adress3,
+      PostalCode: employee.PostalCode,
+      City: employee.City,
+      Country: employee.Country,
+      Establishment: establishment,
+      //Gestion automatique"EmployeeId": "string",
+      StatisticalCode: "",
+      Organization1: "",
+      Organization2: "",
+      Organization3: "",
+      Organization4 : "",
+      FreeSalary1: 0,
+      FreeSalary2: 0,
+      FreeSalary3: 0,
+      FreeSalary4: 0,
+      FreeSalary5: 0,
+      FreeCombo1: "",
+      FreeCombo2: "",
+      FreeCombo3: "",
+      FreeCombo4: "",
+      FreeDate1: "",
+      FreeDate2: "",
+      FreeDate3: "",
+      FreeDate4: "",
+      FreeBool1: true,
+      FreeBool2: true,
+      FreeBool3: true,
+      FreeBool4: true,
+      //ResourceId: "",
+      EntryDate: date,
+      BirthDate: "",
+      SocialNumber: "",
+      CreateContract: false
+    }
+  
+    return newEmployee
+
 }
 
 
